bed0cc7724084947cbcc563426a75700a1cc99ba
Casey Doig <cddoigkn@gmail.com>
2023-12-10 17:50:16 -0600

    user routes and user controllers done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -45,10 +45,34 @@ async function deleteItemById(id) {
   }
 }
 
+async function giveUserAFriend(userid, friendid) {
+  try {
+    const userToChange = await Model.findById(userid);
+    const currentFriends = userToChange.friends;
+    return await Model.findOneAndUpdate(
+      { _id: userid }, { friends: [...currentFriends, friendid] }, { new: true }
+    )
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+async function getRidOfAFriend(userid, friendid) {
+  try {
+    return await Model.findOneAndUpdate(
+      // $pull looks within a property and removes the specified value (found on mongoose website) //
+      { _id: userid }, { $pull: { friends: friendid } }, { new: true }
+    )
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
 module.exports = {
   getAllUsers: getAllItems,
   getUserById: getItemById,
   createUser: createItem,
   updateUserById: updateItemById,
   deleteUserById: deleteItemById,
+  giveUserAFriend
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -12,7 +12,7 @@ router.get("/", async (req, res) => {
   try {
     const payload = await getAllUsers()
     res.status(200).json({ result: "success", payload })
-  } catch(err){
+  } catch (err) {
     res.status(500).json({ result: "error", payload: err.message })
   }
 })
@@ -22,7 +22,7 @@ router.get("/:userid", async (req, res) => {
   try {
     const payload = await getUserById(req.params.userid)
     res.status(200).json({ result: "success", payload })
-  } catch(err){
+  } catch (err) {
     res.status(500).json({ result: "error", payload: err.message })
   }
 })
@@ -32,7 +32,7 @@ router.post("/", async (req, res) => {
   try {
     const payload = await createUser(req.body)
     res.status(200).json({ result: "success", payload })
-  } catch(err){
+  } catch (err) {
     res.status(500).json({ result: "error", payload: err.message })
   }
 })
@@ -42,7 +42,7 @@ router.put("/:userid", async (req, res) => {
   try {
     const payload = await updateUserById(req.params.userid, req.body)
     res.status(200).json({ result: "success", payload })
-  } catch(err){
+  } catch (err) {
     res.status(500).json({ result: "error", payload: err.message })
   }
 })
@@ -52,9 +52,31 @@ router.delete("/:userid", async (req, res) => {
   try {
     const payload = await deleteUserById(req.params.userid)
     res.status(200).json({ result: "success", payload })
-  } catch(err){
+  } catch (err) {
     res.status(500).json({ result: "error", payload: err.message })
   }
 })
+
+// adding a new friend to a user's friend list //
+router.post("/:userid/friends/:friendid", async (req, res) => {
+  try {
+    const payload = await giveUserAFriend(req.params.userid, req.params.friendid)
+    res.status(200).json({ result: "success", payload })
+  } catch (err) {
+    res.status(500).json({ result: "error", payload: err.message })
+  }
+})
+
+// deleting a friend off of a user's friend list
+router.delete("/:userid/friends/:friendid", async (req, res) => {
+  try {
+    const payload = await getRidOfAFriend(req.params.userid, req.params.friendid)
+    res.status(200).json({ result: "success", payload })
+  } catch (err) {
+    res.status(500).json({ result: "error", payload: err.message })
+  }
+})
+
+
 
 module.exports = router;
